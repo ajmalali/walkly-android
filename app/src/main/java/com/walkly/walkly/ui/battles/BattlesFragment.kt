5b66a9f7b5a767117146df4dc5fbb7cb19ee57a6
Abdullah_MGK <Abdullah_MGK@outlook.com>
2019-12-01 01:45:08 +0300

    makes control buttons functional and list dynamic

--- app/src/main/java/com/walkly/walkly/ui/battles/BattlesFragment.kt
+++ app/src/main/java/com/walkly/walkly/ui/battles/BattlesFragment.kt
@@ -4,11 +4,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import android.widget.ArrayAdapter
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.walkly.walkly.R
+import kotlinx.android.synthetic.main.fragment_battles.*
+
 
 class BattlesFragment : Fragment() {
 
@@ -27,5 +29,36 @@ class BattlesFragment : Fragment() {
             textView.text = it
         })*/
         return root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        var list1 = arrayOf(
+            "One",
+            "Two",
+            "Three",
+            "Four",
+            "Five"
+        )
+        var list2 = arrayOf(
+            "One",
+            "Two",
+            "Three"
+        )
+
+        var adapter1 = ArrayAdapter (activity, R.layout.battleboard_item, R.id.label, list1)
+        var adapter2 = ArrayAdapter (activity, R.layout.battleboard_item, R.id.label, list2)
+
+
+        battlesQuestsRadioButtons.setOnCheckedChangeListener { group, checkedId ->
+            if (battlesButton.isChecked) {
+                textView1.text = "Battles"
+                list_view.adapter = adapter1
+            } else if (questsButton.isChecked) {
+                textView1.text = "Quests"
+                list_view.adapter = adapter2
+            }
+        }
     }
 }
