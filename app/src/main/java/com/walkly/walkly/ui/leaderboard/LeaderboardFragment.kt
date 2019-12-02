f5b8b43e2a62b8455a1c9f4fce980b2bf2465498
Ajmal Ali <ajmalsyedali@gmail.com>
2019-12-02 21:05:22 +0300

    Add global and friend leaderboard buttons

--- app/src/main/java/com/walkly/walkly/ui/leaderboard/LeaderboardFragment.kt
+++ app/src/main/java/com/walkly/walkly/ui/leaderboard/LeaderboardFragment.kt
@@ -2,15 +2,12 @@ package com.walkly.walkly.ui.leaderboard
 
 import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
-import androidx.navigation.Navigation
-import androidx.navigation.fragment.findNavController
 
 import com.walkly.walkly.R
 import com.walkly.walkly.databinding.FragmentLeaderboardBinding
@@ -24,7 +21,6 @@ class LeaderboardFragment : Fragment() {
     ): View? {
 
         val binding: FragmentLeaderboardBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_leaderboard, container, false)
-
         leaderboardViewModel = ViewModelProviders.of(this).get(LeaderboardViewModel::class.java)
 
         binding.lifecycleOwner = this
@@ -32,11 +28,15 @@ class LeaderboardFragment : Fragment() {
         val adapter = LeaderboardAdapter()
         binding.leaderboardRecyclerView.adapter = adapter
 
-        leaderboardViewModel.leaderboardItems.observe(viewLifecycleOwner, Observer { list ->
+        leaderboardViewModel.globalLeaderboard.observe(viewLifecycleOwner, Observer { list ->
             list?.let {
                 adapter.submitList(list)
             }
         })
+
+        binding.globalLeaderboardButton.setOnClickListener {
+            leaderboardViewModel.getGlobalLeaderboard()
+        }
 
         return binding.root
     }
--- app/src/main/java/com/walkly/walkly/ui/leaderboard/LeaderboardViewModel.kt
+++ app/src/main/java/com/walkly/walkly/ui/leaderboard/LeaderboardViewModel.kt
@@ -11,35 +11,49 @@ private const val TAG = "LeaderboardViewModel"
 
 class LeaderboardViewModel : ViewModel() {
 
-    // To prevent access from outside, leaderboardItems is set to read-only
-    private val _leaderboardItems = MutableLiveData<List<LeaderboardItem>>()
-    val leaderboardItems: LiveData<List<LeaderboardItem>>
-        get() = _leaderboardItems
+    // To prevent access from outside, both leaderboards are set to read-only
+    private val _globalLeaderboard = MutableLiveData<List<LeaderboardItem>>()
+    val globalLeaderboard: LiveData<List<LeaderboardItem>>
+        get() = _globalLeaderboard
+
+    private val _friendsLeaderboard = MutableLiveData<List<LeaderboardItem>>()
+    val friendsLeaderboard: LiveData<List<LeaderboardItem>>
+        get() = _friendsLeaderboard
 
     private var list = mutableListOf<LeaderboardItem>()
 
+    private val db: FirebaseFirestore = FirebaseFirestore.getInstance()
+
     init {
-        getTopTenUsers()
+        getGlobalLeaderboard()
     }
 
     // Populates the leaderboard with top 10 users based on points
-    fun getTopTenUsers() {
-        val db = FirebaseFirestore.getInstance()
-        db.collection("users")
-            .orderBy("points", Query.Direction.DESCENDING)
-            .limit(10)
-            .get()
-            .addOnSuccessListener { result ->
-                for (document in result) {
-                    val item: LeaderboardItem = document.toObject(LeaderboardItem::class.java).addId(document.id)
-                    list.add(item)
+    fun getGlobalLeaderboard() {
+        if (_globalLeaderboard.value == null) {
+            db.collection("users")
+                .orderBy("points", Query.Direction.DESCENDING)
+                .limit(10)
+                .get()
+                .addOnSuccessListener { result ->
+                    for (document in result) {
+                        val item: LeaderboardItem =
+                            document.toObject(LeaderboardItem::class.java).addId(document.id)
+                        list.add(item)
 
-                    Log.d(TAG, "${document.id} => ${document.data}")
+                        Log.d(TAG, "${document.id} => ${document.data}")
+                    }
+                    _globalLeaderboard.value = list
                 }
-                _leaderboardItems.value = list
-            }
-            .addOnFailureListener { exception ->
-                Log.w(TAG, "Error getting documents.", exception)
-            }
+                .addOnFailureListener { exception ->
+                    Log.w(TAG, "Error getting documents.", exception)
+                }
+        } else {
+            _globalLeaderboard.value = list
+        }
+    }
+
+    fun getFriendsLeaderboard() {
+
     }
 }
