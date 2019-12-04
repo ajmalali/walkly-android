a3397f69cb7179ed798e65ed8083a2cd5f0fa33a
MCapital <moh-attas@hotmail.com>
2019-12-04 12:15:20 +0300

    added the navigation to the battle activity fragment

--- app/src/main/java/com/walkly/walkly/ui/battleactivity/BattleActivityFragment.kt
+++ app/src/main/java/com/walkly/walkly/ui/battleactivity/BattleActivityFragment.kt
@@ -1,4 +1,32 @@
 package com.walkly.walkly.ui.battleactivity
 
-class BattleActivityFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import com.walkly.walkly.R
+import kotlinx.android.synthetic.main.fragment_battle_activity.*
+
+class BattleActivityFragment: Fragment() {
+    lateinit var v: View
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        v = inflater.inflate(R.layout.fragment_battle_activity, container, false)
+        return v
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        leaveBattle.setOnClickListener {
+            view.findNavController().navigate(R.id.action_Battle_Activity_Fragment_to_navigation_map)
+        }
+    }
+
 }
