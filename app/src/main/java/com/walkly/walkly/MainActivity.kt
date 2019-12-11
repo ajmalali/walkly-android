eb516edb182105c51976a835ca3804cd45dd461a
Omar Alghamdi <mr.alghamdi.omar@gmail.com>
2019-12-11 03:23:37 +0300

    initializes offline battle
    - NOT TESTED
    - get equipment, enemy, player neccessary data to start
    - decrease enemy hp by distance
    - decrease player hp by time
    - make enemy image, enemy hp & player hp available to be observed by view

--- app/src/main/java/com/walkly/walkly/MainActivity.kt
+++ app/src/main/java/com/walkly/walkly/MainActivity.kt
@@ -122,21 +122,7 @@ class MainActivity : AppCompatActivity(){
             stamina.observe(this, Observer {stamina ->
                 Log.d("Stamina: ", stamina.toString())
             })
-            var equipment = Equipment().getAllEquipments()
-            Log.d("last LOg", equipment.size.toString())
-            for (equip in equipment)
-                Log.d("last LOg", equip.toString())
 
-            Log.d("getUsedEquipment",Equipment().getUsedEquipment().toString())
-            var equi = Equipment().addNewEquipmentInUserInventory(Equipment("equipment","shield",4,"defense", 1, "uha"))
-            for(i in equi)
-                Log.d("addNewEquipmentser", i.toString())
-            var test1 = Equipment().addNewEquipment(Equipment("equipment","shield",4,"defense", 1, "test"))
-            for(i in test1)
-                Log.d("addNewEquipment1",i.toString())
-            var test2 = Equipment().addNewEquipment(Equipment("asdasd","asdasd",4,"asdasd", 1, "test"))
-            for (i in test2)
-                Log.d("addNewEquipment2",i.toString())
 
         } else {
             auth.addAuthStateListener {
--- app/src/main/java/com/walkly/walkly/offlineBattle/OfflineBattleViewModel.kt
+++ app/src/main/java/com/walkly/walkly/offlineBattle/OfflineBattleViewModel.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.google.firebase.auth.FirebaseAuth
 import com.walkly.walkly.models.Enemy
 import com.walkly.walkly.models.Equipment
 import com.walkly.walkly.models.Player
@@ -19,65 +20,80 @@ class OfflineBattleViewModel (activity: AppCompatActivity, enemy: Enemy) : ViewM
     private val FREQuNCY = 1000L
     // used to convert player level to HP
     private val HP_MULTIPLAYER = 100
-    // used to convert players steps to damage
 
     private val job = Job()
     private val scope = CoroutineScope(Dispatchers.Main + job)
 
-    val activity = activity
+    private lateinit var player: Player
+    private lateinit var distanceUtil: DistanceUtil
+    private val walkedDistance = MutableLiveData<Float>()
 
-    val distanceUtil: DistanceUtil
+    // BAD DESIGN: should get refactored
+    private val stamina = MutableLiveData<Long>()
+    private val auth = FirebaseAuth.getInstance()
 
-    var enemyImage: String
-    private val enemyDamage: Long
-    private val baseEnemyHP: Long
+    private var baseEnemyHP = 0L
+    private var enemyDamage = 0L
+    private var basePlayerHP = 0L
+    private var playerDamage = 0L
+
+    // view observes these
+    val enemyImage = MutableLiveData<String>()
     val enemyHP = MutableLiveData<Long>()
-    private val playerDamage: Long
-    private var basePlayerHP: Long = 0
     val playerHP = MutableLiveData<Long>()
-    private val steps = MutableLiveData<Float>()
+
+
 
     init {
-        enemyDamage = enemy.damage
-        baseEnemyHP= enemy.HP
-        enemyHP.value = baseEnemyHP
-        enemyImage = enemy.image
-        // uncomment after fixing player class design
-        // basePlayerHP = Player().level * HP_MULTIPLAYER
-        // playerHP.value = basePlayerHP
-        playerDamage = Equipment().getUsedEquipment().value
-
-        distanceUtil = DistanceUtil(activity, Calendar.getInstance().timeInMillis, 200L, steps)
-
-        steps.observe(activity, androidx.lifecycle.Observer {
-            enemyHP.value = baseEnemyHP - it.toInt() * playerDamage
+        // BAD DESIGN: should get refactored
+        if (auth.currentUser != null) {
+            player = Player(stamina)
+            // get damage player can do based on equipment
+            player.equipment.observe(activity, androidx.lifecycle.Observer {
+                it.value.observe(activity, androidx.lifecycle.Observer {value ->
+                    playerDamage = value
+                })
+            })
+        }
+        // get the starting player HP
+        player.level.observe(activity, androidx.lifecycle.Observer {
+            basePlayerHP = it * HP_MULTIPLAYER
+            playerHP.value = basePlayerHP
+        })
+        // get the starting enemy HP
+        enemy.HP.observe(activity, androidx.lifecycle.Observer {
+            baseEnemyHP = it
+            enemyHP.value = baseEnemyHP
+        })
+        // get enemy image
+        enemy.image.observe(activity, androidx.lifecycle.Observer {
+            enemyImage.value = it
+        })
+        // get enemy damage
+        enemy.damage.observe(activity, androidx.lifecycle.Observer {
+            enemyDamage = it
         })
 
+        distanceUtil = DistanceUtil(activity, Calendar.getInstance().timeInMillis, 500, walkedDistance)
+
+        // reduce enemy HP by distance walked * equipment value
+        walkedDistance.observe(activity, androidx.lifecycle.Observer {
+            enemyHP.value = (baseEnemyHP - it * playerDamage).toLong()
+        })
+
+        // reduce player HP by time * enemy damage
         scope.launch {
-            damagePlayer(FREQuNCY, enemyDamage)
+            damagePlayer()
         }
 
-    }
-
-
-    fun onBattleCancelled(){
-        TODO()
-    }
-
-    fun onActivityPause(){
-        distanceUtil.stopUpdates()
 
     }
 
-    fun onActivityResume(){
-        distanceUtil.startUpdates()
-    }
-
-    // won't work while app is off
-    suspend fun damagePlayer(freqncy: Long, amount: Long){
-        while (basePlayerHP!! > 0){
-            delay(freqncy)
-            basePlayerHP -= amount
+    // WARNING: won't work while screen is off
+    suspend fun damagePlayer(){
+        while (true){
+            delay(FREQuNCY)
+            basePlayerHP -= enemyDamage
             playerHP.value = basePlayerHP
         }
     }
