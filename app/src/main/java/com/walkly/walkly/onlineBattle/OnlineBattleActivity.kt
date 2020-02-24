b5b4ed21110a1b56216a135fd7c24eac98ddbd09
Ajmal Ali <ajmalsyedali@gmail.com>
2020-02-25 02:12:17 +0300

    Add Join/Host battle UI

---
+++ app/src/main/java/com/walkly/walkly/onlineBattle/OnlineBattleActivity.kt
@@ -0,0 +1,11 @@
+package com.walkly.walkly.onlineBattle
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class OnlineBattleActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
