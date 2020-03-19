bfb7f77419555d9bc4e68c09454ae3178c030bdc
Ajmal Ali <ajmalsyedali@gmail.com>
2020-03-19 17:03:57 +0300

    Fix Offline Battle with new player model (Consumables broken currently)

--- app/src/main/java/com/walkly/walkly/offlineBattle/OfflineBattleActivity.kt
+++ app/src/main/java/com/walkly/walkly/offlineBattle/OfflineBattleActivity.kt
@@ -2,10 +2,14 @@ package com.walkly.walkly.offlineBattle
 
 import android.app.AlertDialog
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
+import androidx.activity.viewModels
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.bumptech.glide.Glide
@@ -13,29 +17,39 @@ import com.walkly.walkly.MainActivity
 import com.walkly.walkly.R
 import com.walkly.walkly.models.Enemy
 import com.walkly.walkly.models.Player
+import com.walkly.walkly.utilities.DistanceUtil
 import kotlinx.android.synthetic.main.fragment_battle_activity.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
+import java.util.*
 
 class OfflineBattleActivity : AppCompatActivity() {
 
-    lateinit var viewModel: OfflineBattleViewModel
-    private lateinit var viewModelFactory: OfflineBattleViewModelFactory
+    val viewModel: OfflineBattleViewModel by viewModels()
 
     private lateinit var loseDialog: AlertDialog
     private lateinit var leaveDialog: AlertDialog
     private lateinit var winDialog: AlertDialog
 
     private lateinit var consumablesBottomSheetDialog: ConsumablesBottomSheetDialog
+    private val walkedDistance = MutableLiveData<Float>()
 
+    private lateinit var distanceUtil: DistanceUtil
 
+    private val job = Job()
+    private val scope = CoroutineScope(Dispatchers.Main + job)
+
+    @RequiresApi(Build.VERSION_CODES.N)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         supportActionBar?.hide()
-
         setContentView(R.layout.fragment_battle_activity)
 
+        // TODO: Refactor this (Use Parcelables)
         val bundle = intent.extras
-        lateinit var enemy: Enemy
+        var enemy: Enemy? = null
         bundle?.let {
             enemy = Enemy(
                 bundle.getLong("enemyLvl"),
@@ -45,21 +59,13 @@ class OfflineBattleActivity : AppCompatActivity() {
             )
         }
 
+        enemy?.let { viewModel.initEnemy(it) }
 
-        consumablesBottomSheetDialog =
-            ConsumablesBottomSheetDialog(this)
+        distanceUtil = DistanceUtil(this, walkedDistance)
 
-
-        viewModelFactory = OfflineBattleViewModelFactory(this, enemy)
-
-//        viewModel = OfflineBattleViewModel(this, enemy)
-        viewModel = ViewModelProviders.of(this, viewModelFactory)
-
-            .get(OfflineBattleViewModel::class.java)
+        consumablesBottomSheetDialog = ConsumablesBottomSheetDialog(this)
 
         use_items.setOnClickListener {
-            //            Toast.makeText(this, "coming in 418", Toast.LENGTH_LONG)
-//                .show()
             consumablesBottomSheetDialog.show(supportFragmentManager, "consumableSheet")
         }
 
@@ -69,9 +75,9 @@ class OfflineBattleActivity : AppCompatActivity() {
         })
 
 
-
         viewModel.playerHP.observe(this, Observer {
-            player_health_bar.progress = it.toInt()
+//            player_health_bar.progress = it.toInt()
+            player_health_bar.setProgress(it, true)
             if (it <= 0) {
                 loseDialog.show()
                 loseDialog.findViewById<Button>(R.id.button1)
@@ -82,7 +88,9 @@ class OfflineBattleActivity : AppCompatActivity() {
         })
 
         viewModel.enemyHP.observe(this, Observer {
-            enemy_health_bar.progress = it.toInt()
+//            enemy_health_bar.progress = it.toInt()
+            enemy_health_bar.setProgress(it, true)
+            // If game ends
             if (it <= 0) {
 //                enemy.level.value?.toInt()?.let { it1 -> Player.updatePoints(it1) }
                 getReward()
@@ -94,24 +102,31 @@ class OfflineBattleActivity : AppCompatActivity() {
             }
         })
 
-        viewModel.enemyImage.observe(this, Observer {
-            val imagename = "boss" + bundle?.getString("enemyId")
-            boss_bitmoji.setImageResource(
-                resources.getIdentifier(
-                    imagename,
-                    "drawable",
-                    this.packageName
-                )
+        // TODO: Use enemy image
+        // get enemy image
+//        val imagename = enemy.image.toString()
+        val imagename = "boss" + bundle?.getString("enemyId")
+        boss_bitmoji.setImageResource(
+            resources.getIdentifier(
+                imagename,
+                "drawable",
+                this.packageName
             )
-        })
+        )
 
-        // NOTE TESTED
+        // NOT TESTED
         // Since starting time is now where the walked distance = 0
+
+        // TODO: Change to Enemy Health / player equipment
         var steps = 0
-        tv_no_of_steps.text = "$steps / 1000"
-        viewModel.walkedDistance.observe(this, Observer {
+        var totalSteps = "$steps / 1000"
+        tv_no_of_steps.text = totalSteps
+
+        walkedDistance.observe(this, Observer {
+            viewModel.damageEnemy(it)
             steps += it.toInt()
-            tv_no_of_steps.text = "$steps / 1000"   // HARD CODED
+            totalSteps = "$steps / 1000"
+            tv_no_of_steps.text = totalSteps
             Log.d("steps = ", it.toString())
         })
 
@@ -141,21 +156,28 @@ class OfflineBattleActivity : AppCompatActivity() {
             .setView(R.layout.dialog_battle_won)
             .create()
 
-        viewModel.startBattle()
+        startBattle()
     }
 
     private fun getReward() {
 
     }
 
-    fun endGame() {
+    private fun startBattle() {
+        scope.launch {
+            viewModel.damagePlayer()
+        }
+    }
+
+    private fun endGame() {
+        job.complete()
         val intent = Intent(this, MainActivity::class.java)
         startActivity(intent)
         finish()
     }
 
     private fun useConsumable(consumableType: String, consumableValue: Int) {
-        when (consumableType.toLowerCase()) {
+        when (consumableType.toLowerCase(Locale.ROOT)) {
             "attack" -> enemy_health_bar.progress -= consumableValue
             "health" -> player_health_bar.progress += consumableValue
         }
--- app/src/main/java/com/walkly/walkly/offlineBattle/OfflineBattleViewModel.kt
+++ app/src/main/java/com/walkly/walkly/offlineBattle/OfflineBattleViewModel.kt
@@ -1,9 +1,6 @@
 package com.walkly.walkly.offlineBattle
 
-import android.app.Activity
 import android.util.Log
-import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Observer
@@ -11,47 +8,40 @@ import androidx.lifecycle.ViewModel
 import com.google.firebase.auth.FirebaseAuth
 import com.walkly.walkly.models.Consumable
 import com.walkly.walkly.models.Enemy
-import com.walkly.walkly.models.Equipment
-import com.walkly.walkly.models.Player
 import com.walkly.walkly.repositories.ConsumablesRepository
+import com.walkly.walkly.repositories.PlayerRepository
 import com.walkly.walkly.utilities.DistanceUtil
 import kotlinx.coroutines.*
-import java.time.temporal.TemporalAmount
-import java.util.*
 
-class OfflineBattleViewModel (activity: AppCompatActivity, enemy: Enemy) : ViewModel(){
+private const val TAG = "OfflineBattleViewModel"
 
-    private val D_TAG = "offline-battle"
+class OfflineBattleViewModel : ViewModel() {
 
-    private val activity = activity
+    // used to specify how frequently enemy hits
+    private val HIT_FREQUENCY = 3000L
 
-    // used to specify how fact enemy hits
-    private val FREQuNCY = 3000L
     // used to convert player level to HP
     private val HP_MULTIPLAYER = 100
 
-    private val job = Job()
-    private val scope = CoroutineScope(Dispatchers.Main + job)
-
-    private lateinit var distanceUtil: DistanceUtil
-    val walkedDistance = MutableLiveData<Float>()
-
-    // BAD DESIGN: should get refactored
-
     private val auth = FirebaseAuth.getInstance()
+    private val currentPlayer = PlayerRepository.getPlayer()
 
-    private var baseEnemyHP = 0.0
+    var enemy: Enemy? = null
+
+    private var baseEnemyHP = -1L
+    private var currentEnemyHp = 0L
+    private var enemyHpPercentage = 100
     private var enemyDamage = 0L
-    private var currentEnemyHp = 0.0
-    private var enemyHpPercentage = 100L
+
     private var basePlayerHP = 1L
-    private var currnetPlayerHP = 0L
+    private var currentPlayerHP = 0L
+    private var playerHpPercentage: Int = 100
     private var playerDamage = 0L
 
     // view observes these
-    val enemyImage = MutableLiveData<String>()
-    val enemyHP = MutableLiveData<Long>()
-    val playerHP = MutableLiveData<Long>()
+    // Health bar values
+    val enemyHP = MutableLiveData<Int>()
+    val playerHP = MutableLiveData<Int>()
 
     private val _consumables = MutableLiveData<List<Consumable>>()
     val consumables: LiveData<List<Consumable>>
@@ -64,73 +54,50 @@ class OfflineBattleViewModel (activity: AppCompatActivity, enemy: Enemy) : ViewM
     init {
 
         getConsumables()
+        // get damage player can do based on equipment
+//        playerDamage = currentPlayer.currentEquipment?.value!!
+        playerDamage = 5L
 
-        // BAD DESIGN: should get refactored
-        if (auth.currentUser != null) {
-
-            // get damage player can do based on equipment
-//            Player.equipment.observe(activity, Observer {
-//                    playerDamage = it.value
-//            })
-        }
         // get the starting player HP
-//        Player.level.observe(activity, Observer {
-//            basePlayerHP = it * HP_MULTIPLAYER
-//            currnetPlayerHP = basePlayerHP
-//            playerHP.value = (currnetPlayerHP / basePlayerHP) * 100
-//            Log.d(D_TAG, "it is = " + it)
-//            Log.d(D_TAG, "base player hp = " + baseEnemyHP)
-//            Log.d(D_TAG,  "current player hp = " + currnetPlayerHP)
-//        })
-        // get the starting enemy HP
-        enemy.HP.observe(activity, Observer {
-            baseEnemyHP = it.toDouble()
-            currentEnemyHp = baseEnemyHP
-            enemyHP.value = baseEnemyHP.toLong()
-        })
-        // get enemy image
-        enemy.image.observe(activity, Observer {
-            enemyImage.value = it
-        })
-        // get enemy damage
-        enemy.damage.observe(activity, Observer {
-            enemyDamage = it
-        })
-
-
-
-        // reduce enemy HP by distance walked * equipment value
-        walkedDistance.observe(activity, Observer {
-            currentEnemyHp -= it * 10
-            enemyHpPercentage = ((currentEnemyHp * 100.0) / baseEnemyHP).toLong()
-            enemyHP.value = enemyHpPercentage
-            Log.d(D_TAG, "distance = " + it)
-        })
-
-        // reduce player HP by time * enemy damage
-
-
-
+        basePlayerHP = currentPlayer.level?.times(HP_MULTIPLAYER) ?: 1
+        currentPlayerHP = basePlayerHP
+        playerHpPercentage = ((currentPlayerHP * 100.0) / basePlayerHP).toInt()
+        playerHP.value = playerHpPercentage
     }
 
-    fun startBattle(){
-        distanceUtil = DistanceUtil(activity, walkedDistance)
+    fun initEnemy(enemy: Enemy) {
+        if (baseEnemyHP == -1L) {
+            // get the starting enemy HP
+//        baseEnemyHP = enemy.HP
+            baseEnemyHP = 100L
+            currentEnemyHp = baseEnemyHP
+            enemyHP.value = currentEnemyHp.toInt()
 
-        scope.launch {
-            damagePlayer()
+            // get enemy damage
+//            enemyDamage = enemy.damage
+            enemyDamage = 1L
         }
+    }
+
+
+    // reduce enemy HP by distance walked * equipment value
+    fun damageEnemy(steps: Float) {
+//        currentEnemyHp -= steps * currentPlayer.currentEquipment?.value!!
+        currentEnemyHp -= steps.toLong()
+        enemyHpPercentage = ((currentEnemyHp * 100.0) / baseEnemyHP).toInt()
+        enemyHP.value = enemyHpPercentage
+        Log.d(TAG, "Current enemy health: $currentEnemyHp")
     }
 
     // WARNING: won't work while screen is off
-    suspend fun damagePlayer(){
-        var playerHppercentage = 100L
-        while (true){
-            delay(FREQuNCY)
-            Log.d(D_TAG, "current player hp = " + currnetPlayerHP)
-            currnetPlayerHP -= enemyDamage
-            playerHppercentage = (currnetPlayerHP * 100) / basePlayerHP
-//            currnetPlayerHP = ((currnetPlayerHP - enemyDamage) * 100) / basePlayerHP
-            playerHP.value = playerHppercentage
+    // reduce player HP by time * enemy damage
+    suspend fun damagePlayer() {
+        while (true) {
+            delay(HIT_FREQUENCY)
+            currentPlayerHP -= enemyDamage
+            playerHpPercentage = ((currentPlayerHP * 100) / basePlayerHP).toInt()
+            playerHP.value = playerHpPercentage
+            Log.d(TAG, "Current player health = $currentPlayerHP")
         }
     }
 
