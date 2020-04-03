ee22c17a7a6aeb735d199a9c4640a96eba8b9323
Omar Alghamdi <mr.alghamdi.omar@gmail.com>
2020-04-03 13:34:23 +0300

    implements more friend list functionalities
    - add friend
    - accept friend
    - reject friend

--- app/src/main/java/com/walkly/walkly/repositories/FriendsRepository.kt
+++ app/src/main/java/com/walkly/walkly/repositories/FriendsRepository.kt
@@ -1,6 +1,7 @@
 package com.walkly.walkly.repositories
 
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FieldValue
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.ktx.toObject
 import com.walkly.walkly.models.Friend
@@ -11,10 +12,14 @@ object FriendsRepository {
     private val uid = FirebaseAuth.getInstance().currentUser?.uid.toString()
     private val userDoc = db.collection("users").document(uid)
     private val friends = mutableListOf<String>()
+    private val friendRquests = mutableListOf<String>()
 
     init {
         userDoc.addSnapshotListener { snapshot, exception ->
-            friends.addAll(0, snapshot?.data?.get("friends") as List<String>)
+            friends.clear()
+            friendRquests.clear()
+            friends.addAll(snapshot?.data?.get("friends") as List<String>)
+            friendRquests.addAll(snapshot?.data?.get("friend-requests") as List<String>)
         }
     }
 
@@ -40,14 +45,58 @@ object FriendsRepository {
             .addOnSuccessListener {
                 users.clear()
                 for (document in it){
+                    // TODO: check pending or accepted
                     if (friends.contains(document.id)){
                         val friend = document.toObject<Friend>().addId(document.id)
-                        // TODO: check pending or accepted
+
                         friend.type = 1
+                        users.add(friend)
+                    } else if (friendRquests.contains(document.id)){
+                        val friend = document.toObject<Friend>().addId(document.id)
+
+                        friend.type = 0
                         users.add(friend)
                     }
                 }
                 callback(users)
             }
+    }
+
+    fun addFriend(id: String, callback: (Boolean) -> Unit) {
+        userDoc.update(
+            "friends", FieldValue.arrayUnion(id)
+        ).addOnSuccessListener {
+            db.collection("users")
+                .document(id)
+                .update(
+                    "friend-request", FieldValue.arrayUnion(uid)
+                )
+                .addOnSuccessListener {
+                    callback(true)
+                }
+        }
+    }
+
+    fun acceptFriend(id: String, callback: (Boolean) -> Unit){
+        userDoc.update(
+            "friends", FieldValue.arrayUnion(id),
+            "friend-requests", FieldValue.arrayRemove(id)
+        ).addOnSuccessListener {
+            callback(true)
+        }
+    }
+
+    fun rejectFriend(id: String, callback: (Boolean) -> Unit){
+        userDoc.update(
+            "friend-requests", FieldValue.arrayRemove(id)
+        ).addOnSuccessListener {
+            db.collection("users")
+                .document(id)
+                .update(
+                    "friends", FieldValue.arrayRemove(uid)
+                ).addOnSuccessListener {
+                    callback(true)
+                }
+        }
     }
 }
